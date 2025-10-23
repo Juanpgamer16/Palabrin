9b59e1a84e8cefab284612f71b538e003029e2cf
Juanpgamer16 <juanpjuanp16@gmail.com>
2025-10-23 01:25:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// Obtiene el botón de AR usando el ID definido en el HTML
 const arButton = document.getElementById('ar-button-js');
 
 function manejarBotonAR() {
-    // Esta alerta se mostrará al usuario justo antes de que se inicie la experiencia AR en el móvil.
-    alert('¡El visor AR se está iniciando! Sigue las instrucciones de tu teléfono para colocar el modelo.');
-    
-    // Aquí puedes añadir cualquier otra funcionalidad de JavaScript que necesites, 
-    // como rastrear que el botón fue presionado (Analytics).
+    // Esto se ejecuta JUSTO antes de que se inicie la experiencia AR en el móvil.
+    alert('¡El visor AR se está iniciando! Usa dos dedos (gesto de pinza) para escalar y reubicar el modelo.');
 }
 
 // Vincula la función 'manejarBotonAR' al evento de 'click' del botón
